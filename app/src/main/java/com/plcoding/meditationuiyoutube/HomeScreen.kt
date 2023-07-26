f0893d57729c5046679aacfd421e540f6610581c
Mark <mark130599@gmail.com>
2023-07-26 19:09:00 +0300

    greeting section

---
+++ app/src/main/java/com/plcoding/meditationuiyoutube/HomeScreen.kt
@@ -0,0 +1,75 @@
+package com.plcoding.meditationuiyoutube
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.plcoding.meditationuiyoutube.ui.theme.DeepBlue
+import com.plcoding.meditationuiyoutube.ui.theme.Typography
+import com.plcoding.meditationuiyoutube.ui.theme.gothicA1
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DeepBlue)
+    ) {
+        Column {
+            GreetingSection()
+        }
+
+    }
+}
+
+@Composable
+fun GreetingSection(
+    name: String = "Mark"
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good Morning, $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "We wish you have a good day",
+                style = MaterialTheme.typography.body1
+            )
+        }
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search Icon",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+
+    }
+}
+
+
