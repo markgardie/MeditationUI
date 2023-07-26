625b24037da25307d6ae6e9d68b168488e89e3a3
Mark <mark130599@gmail.com>
2023-07-26 19:23:23 +0300

    chips section

--- app/src/main/java/com/plcoding/meditationuiyoutube/HomeScreen.kt
+++ app/src/main/java/com/plcoding/meditationuiyoutube/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.plcoding.meditationuiyoutube
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,16 +12,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.plcoding.meditationuiyoutube.ui.theme.ButtonBlue
+import com.plcoding.meditationuiyoutube.ui.theme.DarkerButtonBlue
 import com.plcoding.meditationuiyoutube.ui.theme.DeepBlue
+import com.plcoding.meditationuiyoutube.ui.theme.TextWhite
 import com.plcoding.meditationuiyoutube.ui.theme.Typography
 import com.plcoding.meditationuiyoutube.ui.theme.gothicA1
 
@@ -33,6 +44,7 @@ fun HomeScreen() {
     ) {
         Column {
             GreetingSection()
+            ChipSection(listOf("Sweet sleep", "Insomnia", "Depression"))
         }
 
     }
@@ -70,6 +82,42 @@ fun GreetingSection(
         )
 
     }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+
+    LazyRow {
+        items(chips.size) {
+            Box(
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it) ButtonBlue
+                        else DarkerButtonBlue
+                    )
+                    .padding(15.dp),
+                contentAlignment = Alignment.Center
+            ) {
+
+                Text(
+                    text = chips[it],
+                    color = TextWhite
+                )
+
+            }
+        }
+    }
+
 }
 
 
